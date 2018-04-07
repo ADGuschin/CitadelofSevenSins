5678de344e038096e22986a0e342bd44b576c5b1
Alexander Volkov <tohkoecalo@gmail.com>
2018-04-07 22:02:53 +0500

    хуй

--- Igor 2/shadow/shadow.go
+++ Igor 2/shadow/shadow.go
@@ -1,5 +1,5 @@
 embedded_components {
-  id: "collisionobject"
+  id: "spikes"
   type: "collisionobject"
   data: "collision_shape: \"\"\n"
   "type: COLLISION_OBJECT_TYPE_KINEMATIC\n"
@@ -12,7 +12,7 @@ embedded_components {
   "  shapes {\n"
   "    shape_type: TYPE_BOX\n"
   "    position {\n"
-  "      x: 300.0\n"
+  "      x: 400.0\n"
   "      y: 300.0\n"
   "      z: 0.0\n"
   "    }\n"
@@ -32,26 +32,6 @@ embedded_components {
   "linear_damping: 0.0\n"
   "angular_damping: 0.0\n"
   "locked_rotation: false\n"
-  ""
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
-}
-embedded_components {
-  id: "sprite"
-  type: "sprite"
-  data: "tile_set: \"/hero/hero.atlas\"\n"
-  "default_animation: \"ArmL01\"\n"
-  "material: \"/builtins/materials/sprite.material\"\n"
-  "blend_mode: BLEND_MODE_ALPHA\n"
   ""
   position {
     x: 0.0

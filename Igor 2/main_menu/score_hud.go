265d60665b841ab5d42b48ae70925f8f7be82449
PResurrection <pavel_zakr@mail.ru>
2018-04-09 21:04:41 +0500

    GUI

---
+++ Igor 2/main_menu/score_hud.go
@@ -0,0 +1,15 @@
+components {
+  id: "score_hud"
+  component: "/main_menu/score_hud.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}

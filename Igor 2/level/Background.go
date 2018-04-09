7148de9e86844d8194c13175c2931095ddca67af
Sabadon <38100235+Sabadon@users.noreply.github.com>
2018-04-09 17:55:19 +0500

    ЗАДНИК СДЕЛАЛ
    ГОРА МЕДЛЕННО ДВИЖЕТСЯ НО МОЖНО МЕДЛЕННЕЙ И КАРОЧЕ С РЕСЕТОМ ОНА ОБРАТНО ПРЫГАЕТ, А ТО УЙДЕТ ПОСЛЕ МИЛЛИОНА СМЕРТЕЙ И ВСЕ

--- Igor 2/level/Background.go
+++ Igor 2/level/Background.go
@@ -18,23 +18,3 @@ embedded_components {
     w: 1.0
   }
 }
-embedded_components {
-  id: "sprite1"
-  type: "sprite"
-  data: "tile_set: \"/level/Background.atlas\"\n"
-  "default_animation: \"background_1-4_3\"\n"
-  "material: \"/builtins/materials/sprite.material\"\n"
-  "blend_mode: BLEND_MODE_ALPHA\n"
-  ""
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
-}

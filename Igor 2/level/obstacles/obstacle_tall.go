95dbd2ebbf26f7770632c0eeb66e1e0d57771933
Sabadon <38100235+Sabadon@users.noreply.github.com>
2018-04-13 21:33:32 +0500

    ОДНОСТОРОННИЕ ПЛОТФОРМЫ В НЕУЯЗВИМОСТЫ
    короче он когда головой ебоньк
    становится безтелесным псом, но вот только верхняя часть платформ по прежнему его кас(кос)ается

--- Igor 2/level/obstacles/obstacle_tall.go
+++ Igor 2/level/obstacles/obstacle_tall.go
@@ -61,6 +61,26 @@ embedded_components {
   }
 }
 embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/level/level.atlas\"\n"
+  "default_animation: \"\\321\\202\\320\\260\\320\\273\\320\\273\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 24.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
   id: "side"
   type: "collisionobject"
   data: "collision_shape: \"\"\n"
@@ -88,7 +108,7 @@ embedded_components {
   "    count: 3\n"
   "  }\n"
   "  data: 1.9805\n"
-  "  data: 20.0\n"
+  "  data: 20.782\n"
   "  data: 10.0\n"
   "}\n"
   "linear_damping: 0.0\n"
@@ -98,26 +118,6 @@ embedded_components {
   position {
     x: 0.0
     y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
-}
-embedded_components {
-  id: "sprite"
-  type: "sprite"
-  data: "tile_set: \"/level/level.atlas\"\n"
-  "default_animation: \"\\321\\202\\320\\260\\320\\273\\320\\273\"\n"
-  "material: \"/builtins/materials/sprite.material\"\n"
-  "blend_mode: BLEND_MODE_ALPHA\n"
-  ""
-  position {
-    x: 0.0
-    y: 24.0
     z: 0.0
   }
   rotation {

03b636ffd6db06105afc085234ccbb701c66e968
PResurrection <pavel_zakr@mail.ru>
2018-04-15 02:00:24 +0500

    OH MY SOUND!!!
    Sound was added to the game

---
+++ Igor 2/sounds/sound.go
@@ -0,0 +1,35 @@
+components {
+  id: "sound"
+  component: "/sounds/sound.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "maintheme"
+  type: "sound"
+  data: "sound: \"/sounds/maintheme.wav\"\n"
+  "looping: 0\n"
+  "group: \"master\"\n"
+  "gain: 1.0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}

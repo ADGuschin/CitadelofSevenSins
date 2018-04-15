28282f06698d2523143fe20f97219bb0e83429d4
PResurrection <pavel_zakr@mail.ru>
2018-04-15 17:28:42 +0500

    Added soundoff button
    Just the first version, it will be reworked later.

--- Igor 2/gui/gui_gameobjects/hud.go
+++ Igor 2/gui/gui_gameobjects/hud.go
@@ -31,3 +31,21 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "level2_proxy"
+  type: "collectionproxy"
+  data: "collection: \"/level/level2.collection\"\n"
+  "exclude: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}

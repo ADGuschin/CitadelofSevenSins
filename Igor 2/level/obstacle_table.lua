84d4db50b815b6e19748d00045562612e8330368
cserox <38156805+cserox@users.noreply.github.com>
2018-04-23 19:03:02 +0500

    #24
    #24 баг пофикшен
    окончательно пофикшен баг со спавном платформ, можно свободно добавлять любые препятствия

--- Igor 2/level/obstacle_table.lua
+++ Igor 2/level/obstacle_table.lua
@@ -3,10 +3,18 @@
 -- require "my_directory.my_file"
 -- in any script using the functions.
 obstacle_table = {
-	["init"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top", "#train_home"},
-	["#obstacle_small"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top", "#train_home"},
-	["#obstacle_tall"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top", "#train_home"},
-	["#obstacle_top"] = {"#obstacle_small", "#obstacle_tall", "#train_home"},
-	["#train_home"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top"}
-	--["#coll_1"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top"}
+	["init"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top", "#train_home", "#coll_1"},
+	["#obstacle_small"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top", "#train_home", "#coll_1"},
+	["#obstacle_tall"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top", "#train_home", "#coll_1"},
+	["#obstacle_top"] = {"#obstacle_small", "#obstacle_tall", "#train_home", "#coll_1"},
+	["#train_home"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top", "#coll_1"},
+	["#coll_1"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top"}
+}
+
+grid_table = {
+	["#obstacle_small"] = 600, 
+	["#obstacle_tall"] = 600,
+	["#obstacle_top"] = 600,
+	["#train_home"] = 1000,
+	["#coll_1"] = 1200
 }

27730f9f252c825e7387229cc3d3ccd4b3b905d1
PResurrection <pavel_zakr@mail.ru>
2018-04-28 12:46:40 +0500

    #23 #30
    #23 Добавил новый пресет
    #30 Начал делать режим обучения

--- Igor 2/level/obstacle_table.lua
+++ Igor 2/level/obstacle_table.lua
@@ -3,13 +3,14 @@
 -- require "my_directory.my_file"
 -- in any script using the functions.
 obstacle_table = {
-	["init"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top", "#train_home", "#coll_1"},
-	["#obstacle_small"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top", "#train_home", "#coll_1", "#obstacle_jumps"},
-	["#obstacle_tall"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top", "#train_home", "#coll_1","#obstacle_jumps"},
-	["#obstacle_top"] = {"#obstacle_small", "#obstacle_tall", "#train_home", "#coll_1", "#obstacle_jumps"},
-	["#train_home"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top", "#coll_1"},
+	["init"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top", "#train_home", "#coll_1", "#coll_2"},
+	["#obstacle_small"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top", "#train_home", "#coll_1", "#obstacle_jumps", "#coll_2"},
+	["#obstacle_tall"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top", "#train_home", "#coll_1","#obstacle_jumps", "#coll_2"},
+	["#obstacle_top"] = {"#obstacle_small", "#obstacle_tall", "#train_home", "#coll_1", "#obstacle_jumps", "#coll_2"},
+	["#train_home"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top", "#coll_1", "#coll_2"},
 	["#coll_1"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top"},
-	["#obstacle_jumps"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top", "#coll_1"}
+	["#obstacle_jumps"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top", "#coll_1", "#coll_2"},
+	["#coll_2"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top", "#train_home", "#obstacle_jumps"}
 }
 
 grid_table = {
@@ -19,5 +20,6 @@ grid_table = {
 	["#train_home"] = 1000,
 	["#coll_1"] = 1200,
 	["#obstacle_jumps"] = 3000,
-	["#coll_1"] = 1450
+	["#coll_1"] = 1450,
+	["#coll_2"] = 1600
 }

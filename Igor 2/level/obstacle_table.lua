6af6111e5b89684688f9a72deafa896edc33a45f
PResurrection <pavel_zakr@mail.ru>
2018-04-28 19:40:14 +0500

    #23
    Добавил новые пресеты, которые добавляют желание и возможность (и обязанность) держаться не только на земле, но и на высоте, запрыгивая на препятствия

--- Igor 2/level/obstacle_table.lua
+++ Igor 2/level/obstacle_table.lua
@@ -3,23 +3,29 @@
 -- require "my_directory.my_file"
 -- in any script using the functions.
 obstacle_table = {
-	["init"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top", "#train_home", "#coll_1", "#coll_2"},
-	["#obstacle_small"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top", "#train_home", "#coll_1", "#obstacle_jumps", "#coll_2"},
-	["#obstacle_tall"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top", "#train_home", "#coll_1","#obstacle_jumps", "#coll_2"},
-	["#obstacle_top"] = {"#obstacle_small", "#obstacle_tall", "#train_home", "#coll_1", "#obstacle_jumps", "#coll_2"},
-	["#train_home"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top", "#coll_1", "#coll_2"},
-	["#coll_1"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top"},
-	["#obstacle_jumps"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top", "#coll_1", "#coll_2"},
-	["#coll_2"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top", "#train_home", "#obstacle_jumps"}
+	["init"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top", "#train_home", "#coll_1", "#coll_2", "#coll_3"},
+	["#obstacle_small"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top", "#train_home", "#coll_1", "#obstacle_jumps", "#coll_2", "#coll_3"},
+	["#obstacle_tall"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top", "#train_home", "#coll_1","#obstacle_jumps", "#coll_2", "#coll_3.1", "#coll_3.2"},
+	["#obstacle_top"] = {"#obstacle_small", "#obstacle_tall", "#train_home", "#coll_1", "#obstacle_jumps", "#coll_2", "#coll_3.1", "#coll_3.2"},
+	["#train_home"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top", "#coll_1", "#coll_2", "#coll_3.1", "#coll_3.2"},
+	["#coll_1"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top", "#coll_3.1", "#coll_3.2"},
+	["#obstacle_jumps"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top", "#coll_1", "#coll_2", "#coll_3"},
+	["#coll_2"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top", "#train_home", "#obstacle_jumps", "#coll_3"},
+	["#coll_3"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top", "#train_home", "#coll_1", "#obstacle_jumps", "#coll_2"},
+	["#coll_3.1"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top", "#train_home", "#coll_1", "#obstacle_jumps", "#coll_2"},
+	["#coll_3.2"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top", "#train_home", "#coll_1", "#obstacle_jumps", "#coll_2", "#coll_3.1"}
 }
 
 grid_table = {
 	["#obstacle_small"] = 600, 
-	["#obstacle_tall"] = 600,
-	["#obstacle_top"] = 600,
-	["#train_home"] = 1000,
+	["#obstacle_tall"] = 400,
+	["#obstacle_top"] = 400,
+	["#train_home"] = 900,
 	["#coll_1"] = 1200,
 	["#obstacle_jumps"] = 3000,
 	["#coll_1"] = 1450,
-	["#coll_2"] = 1600
+	["#coll_2"] = 1600,
+	["#coll_3"] = 1550,
+	["#coll_3.1"] = 1550,
+	["#coll_3.2"] = 1550
 }

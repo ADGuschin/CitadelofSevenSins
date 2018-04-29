000d94350de630f6d69157088b5acf7e48919429
PResurrection <pavel_zakr@mail.ru>
2018-04-29 18:52:59 +0500

    #20 #23
    #20 Разделил по папкам геймобджекты и коллекции пресетов, перенес шрифты в папку со шрифтами
    #23 Добавил пресет с трубами чисто для теста, можно менять и дорабатывать

--- Igor 2/level/obstacle_table.lua
+++ Igor 2/level/obstacle_table.lua
@@ -3,17 +3,18 @@
 -- require "my_directory.my_file"
 -- in any script using the functions.
 obstacle_table = {
-	["init"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top", "#train_home", "#coll_1", "#coll_2"},
-	["#obstacle_small"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top", "#train_home", "#coll_1", "#obstacle_jumps", "#coll_2", "#coll_3"},
-	["#obstacle_tall"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top", "#train_home", "#coll_1","#obstacle_jumps", "#coll_2", "#coll_3.1", "#coll_3.2"},
-	["#obstacle_top"] = {"#obstacle_small", "#obstacle_tall", "#train_home", "#coll_1", "#obstacle_jumps", "#coll_2", "#coll_3.1", "#coll_3.2"},
-	["#train_home"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top", "#coll_1", "#coll_2", "#coll_3.1", "#coll_3.2"},
-	["#coll_1"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top", "#coll_3.1", "#coll_3.2"},
-	["#obstacle_jumps"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top", "#coll_1", "#coll_2", "#coll_3"},
-	["#coll_2"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top", "#train_home", "#obstacle_jumps", "#coll_3"},
-	["#coll_3"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top", "#train_home", "#coll_1", "#obstacle_jumps", "#coll_2"},
-	["#coll_3.1"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top", "#train_home", "#coll_1", "#obstacle_jumps", "#coll_2"},
-	["#coll_3.2"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top", "#train_home", "#coll_1", "#obstacle_jumps", "#coll_2", "#coll_3.1"}
+	["init"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top", "#train_home", "#coll_1", "#coll_2", "#obstacle_pipes"},
+	["#obstacle_small"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top", "#train_home", "#coll_1", "#obstacle_jumps", "#coll_2", "#coll_3", "#obstacle_pipes"},
+	["#obstacle_tall"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top", "#train_home", "#coll_1","#obstacle_jumps", "#coll_2", "#coll_3.1", "#coll_3.2", "#obstacle_pipes"},
+	["#obstacle_top"] = {"#obstacle_small", "#obstacle_tall", "#train_home", "#coll_1", "#obstacle_jumps", "#coll_2", "#coll_3.1", "#coll_3.2", "#obstacle_pipes"},
+	["#train_home"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top", "#coll_1", "#coll_2", "#coll_3.1", "#coll_3.2", "#obstacle_pipes"},
+	["#coll_1"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top", "#coll_3.1", "#coll_3.2", "#obstacle_pipes"},
+	["#obstacle_jumps"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top", "#coll_1", "#coll_2", "#coll_3", "#obstacle_pipes"},
+	["#coll_2"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top", "#train_home", "#obstacle_jumps", "#coll_3", "#obstacle_pipes"},
+	["#coll_3"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top", "#train_home", "#coll_1", "#obstacle_jumps", "#coll_2", "#obstacle_pipes"},
+	["#coll_3.1"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top", "#train_home", "#coll_1", "#obstacle_jumps", "#coll_2", "#obstacle_pipes"},
+	["#coll_3.2"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top", "#train_home", "#coll_1", "#obstacle_jumps", "#coll_2", "#coll_3.1", "#obstacle_pipes"},
+	["#obstacle_pipes"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top", "#train_home", "#coll_1", "#obstacle_jumps", "#coll_2", "#coll_3", "#obstacle_pipes"}
 }
 
 grid_table = {
@@ -27,5 +28,6 @@ grid_table = {
 	["#coll_2"] = 1280,
 	["#coll_3"] = 1240,
 	["#coll_3.1"] = 1240,
-	["#coll_3.2"] = 1240
+	["#coll_3.2"] = 1240,
+	["#obstacle_pipes"] = 1000
 }

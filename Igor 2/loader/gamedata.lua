f975a1964178c00d790fbe1f85885671c19f6020
PResurrection <pavel_zakr@mail.ru>
2018-04-30 16:59:04 +0500

    #32
    Переосмыслил и переделал всю суть загружчика геймдаты, подкорректировал баланс препятствий

---
+++ Igor 2/loader/gamedata.lua
@@ -0,0 +1,44 @@
+local save_file = sys.get_save_file("Citadel of seven sins", "gamedata")
+
+shared_data = {
+	player = {
+		highscore = 0,
+		score = 0
+	},
+	data = {
+		sound_state = true,
+		training_state = false,
+		autorestart_state = false,
+	}
+}
+
+function save_gamedata(self)
+	-- Save gamedata
+	
+	if sys.save(save_file, shared_data) then
+		print("Геймдата сохранена")
+		pprint(shared_data)
+	else
+		print("Геймдата не сохранена")
+	end
+end
+
+function load_gamedata(self)
+	--	Load gamedata
+	
+	local gamedata = sys.load(save_file)
+
+	if next(gamedata) then
+		print("Геймдата загружена")
+		pprint(gamedata)
+		
+		shared_data.player.highscore = gamedata.player.highscore
+		
+		shared_data.data.sound_state = gamedata.data.sound_state
+		shared_data.data.training_state = gamedata.data.training_state
+		shared_data.data.autorestart_state = gamedata.data.autorestart_state
+	else
+		print("Геймдата пуста")
+		sys.save(save_file, shared_data)
+	end
+end

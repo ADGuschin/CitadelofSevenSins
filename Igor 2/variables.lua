e0d9517548f5e9aba22fc26dda4daa8e80215e03
PResurrection <pavel_zakr@mail.ru>
2018-04-30 12:29:18 +0500

    #30 #29 #20 #11
    #30 Теперь в настройках есть кнопка включения/отключения режима тренировки в начале игры, сам режим еще не доработан
    #29 Теперь новый рекорд выводится на экран смерти, задачу закрываю
    #11 Немного подкорректировал громкость звукового сопровождения
    
    Также подкорректировал размеры ГУИ, чтобы на любых разрешениях выглядело приемлемо

--- Igor 2/variables.lua
+++ Igor 2/variables.lua
@@ -1,12 +1,14 @@
--- Put functions in this file to use them in several other scripts.
--- To get access to the functions, you need to put:
--- require "my_directory.my_file"
--- in any script using the functions.
+--Логические переменные
 sound = true
 in_game = false
 autorestart_state = false
 pause_state = false
+training_state = false
+--Костыль
+deathscreen_state = false
 
+
+--Числовые переменные
 gravity = -65
 slide_cooldown_max = 15
 jump_takeoff_speed = 1260
@@ -16,10 +18,6 @@ max_impact_speed = 1400
 catch_up_speed = 40
 jump_break_speed = -2000
 
---Костыль
-deathscreen_state = false
 
 --Переменные для включения логов
-gui_log = true
-
-training_state = false
+gui_log = false

46b5116602d0603fe04bdd289fa373efd609f660
PResurrection <pavel_zakr@mail.ru>
2018-05-02 01:41:29 +0500

    #32 #15 #27
    Теперь есть загрузочный экран, куда нужно вставить наше лого)
    Заливаю апк

--- Igor 2/main_menu/collection_proxy_content.lua
+++ Igor 2/main_menu/collection_proxy_content.lua
@@ -1,7 +1,9 @@
 collection_proxies =
 {
+	[1] = "#logo_proxy",
 	-- This proxy is loaded with the launching of the game
-	[1] = "#main_menu_proxy",
+	[2] = "#main_menu_proxy",
 	-- This proxy is loaded when 'Start game' is chosen in the main menu (consider it as the first level) 
-	[2] = "#main_proxy",
+	[3] = "#main_proxy"
+
 }

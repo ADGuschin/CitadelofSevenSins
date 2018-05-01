c7db6c71480a23cd9f6b5831e8ab39ea2721bd68
PResurrection <pavel_zakr@mail.ru>
2018-05-01 18:33:23 +0500

    #33
    Теперь два разных трека в меню и в игре

--- Igor 2/sounds/sound.go
+++ Igor 2/sounds/sound.go
@@ -14,9 +14,29 @@ components {
   }
 }
 embedded_components {
-  id: "maintheme"
+  id: "gametheme"
   type: "sound"
-  data: "sound: \"/sounds/Dimooon.wav\"\n"
+  data: "sound: \"/sounds/coss game.wav\"\n"
+  "looping: 1\n"
+  "group: \"master\"\n"
+  "gain: 1.0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "menutheme"
+  type: "sound"
+  data: "sound: \"/sounds/coss menu.wav\"\n"
   "looping: 1\n"
   "group: \"master\"\n"
   "gain: 1.0\n"

9386327ebcfbf7c1a5430b1b10a9024db48b2d86
Alexander Volkov <tohkoecalo@gmail.com>
2018-05-04 19:35:46 +0500

    кран вкинул

--- Igor 2/level/obstacle_table.lua
+++ Igor 2/level/obstacle_table.lua
@@ -6,6 +6,7 @@ obstacle_table = {
 	["#obstacle_small"] = {"#obstacle_tall", "#obstacle_top", "#train_home", "#coll_1", "#obstacle_jumps", "#coll_2", "#coll_3", "#obstacle_pipes","#obstacle_car"},
 	["#obstacle_tall"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top", "#train_home", "#coll_1","#obstacle_jumps", "#coll_2", "#coll_3.1", "#coll_3.2", "#obstacle_pipes", "#obstacle_car"},
 	["#obstacle_top"] = {"#obstacle_small", "#obstacle_tall", "#train_home", "#coll_1", "#obstacle_jumps", "#coll_2", "#coll_3.1", "#coll_3.2", "#obstacle_pipes", "#obstacle_car"},
+	["#obstacle_kran"] = {"#obstacle_small", "#obstacle_tall", "#train_home", "#coll_1", "#obstacle_jumps", "#coll_2", "#coll_3.1", "#coll_3.2", "#obstacle_pipes", "#obstacle_car"},
 	["#train_home"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top", "#coll_1", "#coll_2", "#coll_3.1", "#coll_3.2", "#obstacle_pipes", "#obstacle_car"},
 	["#coll_1"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top", "#obstacle_jumps", "#coll_3.1", "#coll_3.2", "#obstacle_pipes", "#obstacle_car"},
 	["#obstacle_jumps"] = {"#obstacle_small", "#obstacle_tall", "#obstacle_top", "#coll_1", "#obstacle_jumps", "#coll_2", "#coll_3", "#obstacle_pipes", "#obstacle_car"},
@@ -31,5 +32,6 @@ grid_table = {
 	["#coll_3.1"] = 1240,
 	["#coll_3.2"] = 1240,
 	["#obstacle_pipes"] = 1400,
-	["#obstacle_car"] = 480
+	["#obstacle_car"] = 480,
+	["#obstacle_kran"] = 320
 }

9eb0adb9ed0c798e130c6095973bb2add2b8ddfb
Sabadon <38100235+Sabadon@users.noreply.github.com>
2018-05-06 15:16:29 +0500

    #42

--- Igor 2/variables.lua
+++ Igor 2/variables.lua
@@ -22,3 +22,8 @@ max_speed = 2000
 
 --Переменные для включения логов
 gui_log = false
+--Индекс нынешнего уровня
+index_of_current_level = 1 --Счет идет с 1
+--список уровней
+list_of_levels = {"level"}
+current_level = nil

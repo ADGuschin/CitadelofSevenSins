7f9ead6d0db49399bdfe8918a2a9015a52bb8213
cserox <38156805+cserox@users.noreply.github.com>
2018-05-10 19:46:47 +0500

    звуки монеток

--- Igor 2/hero_new/sfx.go
+++ Igor 2/hero_new/sfx.go
@@ -133,3 +133,23 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "coin"
+  type: "sound"
+  data: "sound: \"/hero_new/sfx/coin.wav\"\n"
+  "looping: 0\n"
+  "group: \"master\"\n"
+  "gain: 1.0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}

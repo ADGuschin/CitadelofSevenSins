a6600d0903b35c8ce1357347a36d8c06596acf73
PResurrection <pavel_zakr@mail.ru>
2018-10-16 19:52:30 +0500

    Еще немного shop

--- Citadel of Seven Sins/variables.lua
+++ Citadel of Seven Sins/variables.lua
@@ -53,7 +53,7 @@ language = {"EN", "РУ"}
 language_state = 1
 
 skins = {
-	type = 2,
+	type = 1,
 	names = { "player_runfast_black", "player_fast_right", "test", "test", "test", "test", "test", "test" },
 	shop = { true, false, false, false, false, false, false, false },
 	cost = { 0, 50, 100, 150, 250, 300, 350, 400}

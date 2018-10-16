35c555d3e16b0ea5a681abcd25d1d167eb30a68c
PResurrection <pavel_zakr@mail.ru>
2018-10-16 23:09:23 +0500

    См. описание
    Трек Серого, переделал лоадер и файл сохранений, магазин теперь доступен на двух языках, теперь покупка скинов сохраняется в датафайле

--- Citadel of Seven Sins/variables.lua
+++ Citadel of Seven Sins/variables.lua
@@ -5,7 +5,6 @@ autorestart_state = false
 pause_state = false
 training_state = true
 
-
 --Костыль
 deathscreen_state = false
 next_try_is_training = false
@@ -22,7 +21,6 @@ coins = 0
 current_level_coins = 0
 diamonds = 0
 
-
 --Прогресс
 current_progress = 0
 max_level_progress = {
@@ -34,23 +32,20 @@ current_boss_progress = 0
 max_boss_progress = {
 	["level_1"] = 10000
 }
+
 --Переменные для включения логов
-gui_log = false
+gui_log = true
+
 --Индекс нынешнего уровня
 index_of_current_level = 1 --Счет идет с 1
+
 --Список уровней
 list_of_levels = {"level_1"}
 current_level = nil
 
 --Текущий уровень прогресса
-player_level = 2
+player_level = 1
 
---Настройки языка
-language = {"EN", "РУ"}
-language_state = 1
-
---current_skin = "black"
-current_skin_index = 1 
 skins = {
 	type = 1,
 	names = { "player_runfast_black", "player_fast_right", "test", "test", "test", "test", "test", "test" },
@@ -66,45 +61,52 @@ skins = {
 --Стоимость лутбоксов
 lootboxes_cost = { 50, 100, 150, 200, 250, 300, 350, 400}
 
+--Настройки языка
+language_state = 1
+language = {"EN", "РУ"}
+
 --Локализация
 localization = 	{
-	death_screen =  {
-		restart = { "Restart", "Заново" },
-		menu = { "Menu", "Меню" },
-		death_message = { "You died ^_^", "Ты умер ^_^" },
-		continue_text = { "Continue", "Продолжить" },
-		notenough_text = {"Not enough money :)", "Не достаточно монет :)"},
-		score_message = {"Your score:", "Счет:"},
-		highscore_message = {"NEW HIGHSCORE:", "НОВЫЙ РЕКОРД:"}
-	},
-	pause_screen =  {
-		resume = { "Resume", "Продолжить" },
-		menu = { "Menu", "Меню" }
-	},
-	score = 		{
-		score_text = { "Score:", "Счет:" },
-		highscore_text = { "Highscore:", "Рекорд:" }
-	},
-	settings = 		{
-		language = { "Language:", "Язык:" },
-		training = { "Tutorial:", "Обучение:" },
-		autorestart = { "Autorestart:", "Авторестарт:" },
-		logical_yes = {"yes", "да"},
-		logical_no = {"no", "нет"}
-	},
-	shop = 			{
-		price = { "Buy:", "Купить:" },
-		donate = { "DONATION:", "ДОНАТ:" },
-		back = { "Back", "Назад"}
-	},
-	main_menu = {
-		start = { "Start", "Cтарт"},
-		exit = { "Quit", "Выход"},
-		level = { "Level ", "Уровень "},
-		overrun_mode = { "Overrun", "Заново"},
-		endless_mode = { "Endless", "Бесконечно"},
-		back = { "Back", "Назад"},
-		comingsoon = { "Coming soon..", "Скоро.."}
-	}
-
+					death_screen =  {
+										restart = { "Restart", "Заново" },
+										menu = { "Menu", "Меню" },
+										death_message = { "You died ^_^", "Ты умер ^_^" },
+										continue_text = { "Continue", "Продолжить" },
+										notenough_text = {"Not enough money :)", "Не достаточно монет :)"},
+										score_message = {"Your score:", "Счет:"},
+										highscore_message = {"NEW HIGHSCORE:", "НОВЫЙ РЕКОРД:"}
+					},
+					pause_screen =  {
+										resume = { "Resume", "Продолжить" },
+										menu = { "Menu", "Меню" }
+					},
+					score = 		{
+										score_text = { "Score:", "Счет:" },
+										highscore_text = { "Highscore:", "Рекорд:" }
+					},
+					settings = 		{
+										language = { "Language:", "Язык:" },
+										training = { "Tutorial:", "Обучение:" },
+										autorestart = { "Autorestart:", "Авторестарт:" },
+										logical_yes = {"yes", "да"},
+										logical_no = {"no", "нет"}
+					},
+					shop = 			{
+										price = { "Buy:", "Купить:" },
+										select = { "Select", "Выбрать" },
+										selected = { "Selected", "Выбран" },
+										skins = { "Skins", "Скины" },
+										boxes = { "Boxes", "Лутбоксы" },
+										donate = { "DONATION:", "ДОНАТ:" },
+										back = { "Back", "Назад"}
+					},
+					main_menu = {
+										start = { "Start", "Cтарт"},
+										exit = { "Quit", "Выход"},
+										level = { "Level ", "Уровень "},
+										overrun_mode = { "Overrun", "Заново"},
+										endless_mode = { "Endless", "Бесконечно"},
+										back = { "Back", "Назад"},
+										comingsoon = { "Coming soon..", "Скоро.."}
+					}
 }
